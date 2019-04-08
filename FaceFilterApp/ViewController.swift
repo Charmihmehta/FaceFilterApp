7538ec259b7969741651713ed60f1c57e7c2257e
Charmi Mehta <charmimehta@Charmis-MacBook-Pro.local>
2019-04-08 10:20:02 -0400

    Initial push

--- FaceFilterApp/ViewController.swift
+++ FaceFilterApp/ViewController.swift
@@ -12,11 +12,15 @@ import ARKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var sceneView: ARSCNView!
-    let config = ARWorldTrackingConfiguration()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        sceneView.delegate = self     }
+        sceneView.delegate = self
+        guard ARFaceTrackingConfiguration.isSupported else {
+            fatalError("Face tracking is not supported on this device")
+        }
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
@@ -29,11 +33,6 @@ class ViewController: UIViewController {
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
-        // 1
-        guard ARFaceTrackingConfiguration.isSupported else {
-            fatalError("Face tracking is not supported on this device")
-        }
         sceneView.session.pause()
     }
     
@@ -42,35 +41,23 @@ class ViewController: UIViewController {
 extension ViewController: ARSCNViewDelegate {
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
         
-        // 3
         guard let device = sceneView.device else {
             return nil
         }
-        
-        // 4
+
         let faceGeometry = ARSCNFaceGeometry(device: device)
-        
-        // 5
         let node = SCNNode(geometry: faceGeometry)
-        
-        // 6
         node.geometry?.firstMaterial?.fillMode = .lines
-        
-        // 7
         return node
     }
     func renderer(
         _ renderer: SCNSceneRenderer,
         didUpdate node: SCNNode,
         for anchor: ARAnchor) {
-        
-        // 2
         guard let faceAnchor = anchor as? ARFaceAnchor,
             let faceGeometry = node.geometry as? ARSCNFaceGeometry else {
                 return
         }
-        
-        // 3
         faceGeometry.update(from: faceAnchor.geometry)
     }
     
