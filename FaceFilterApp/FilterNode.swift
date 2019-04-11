fd0d0f43552a8ce196418b46c201bd61299d7cec
Abita Shiney <abitashiney@abitas-macbook-pro.local>
2019-04-10 21:29:46 -0400

    Modified filter effects

--- FaceFilterApp/FilterNode.swift
+++ FaceFilterApp/FilterNode.swift
@@ -17,17 +17,41 @@ class FilterNode: SCNNode {
         self.options = options
         
         super.init()
+//
+//        guard let image = UIImage(named: "nose1") else { return }
+//        let pixelWidth = image.size.width * image.scale
+//        let pixelHeight = image.size.height * image.scale
+//        print(pixelWidth, pixelHeight)
+//
+//        let iphone7PlusPixelsPerInch: CGFloat = 326
+//
+//        //2. To Get The Image Size In Inches We Need To Divide By The PPI
+//        let inchWidth = pixelWidth/iphone7PlusPixelsPerInch
+//        let inchHeight = pixelHeight/iphone7PlusPixelsPerInch
+//
+//        //3. Calculate The Size In Metres (There Are 2.54 Cm's In An Inch)
+//        let widthInMetres = (inchWidth * 2.54) / 100
+//        let heightInMeters = (inchHeight * 2.54) / 100
+//
+//        let noseSize = SCNPlane(width: widthInMetres, height: heightInMeters)
+//        noseSize.firstMaterial?.diffuse.contents = image
+//        noseSize.firstMaterial?.isDoubleSided = true
+       
+      //  geometry = noseSize
         
         let plane = SCNPlane(width: width, height: height)
         plane.firstMaterial?.diffuse.contents = UIImage(named: options.first!)
         plane.firstMaterial?.isDoubleSided = true
         
         geometry = plane
+        
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+
 }
 
 // MARK: - Custom functions
@@ -40,6 +64,7 @@ extension FilterNode {
     }
     
     func next() {
+        
         index = (index + 1) % options.count
         
         if let plane = geometry as? SCNPlane {
--- FaceFilterApp/ViewController.swift
+++ FaceFilterApp/ViewController.swift
@@ -12,10 +12,10 @@ import ARKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var sceneView: ARSCNView!
-    let noseOptions = ["nose1","nose4","nose5"]
-    let eyeOptions = ["eye1","eye2","eye4"]
-    let mouthOptions = ["mouth1", "mustache","mouth4"]
-    let hatOptions = ["hat2", "hat1", "head1", "hat3", "head2","head4","head5","head7","head9"]
+    let noseOptions = ["nose4","nose3","nose2"]
+    let eyeOptions = ["eye4","eye2","eye5","eye7"]
+    let mouthOptions = ["mouth7","mustache","mouth4","mouth8"]
+    let hatOptions = ["hat1", "hat2","head3","head8","head9","head10"]
     let features = ["nose", "leftEye", "rightEye", "mouth", "hat"]
     let featureIndices = [[8], [1064], [42], [24, 25], [20]]
 
@@ -115,27 +115,32 @@ extension ViewController: ARSCNViewDelegate {
         let node = SCNNode(geometry: faceGeometry)
         node.geometry?.firstMaterial?.fillMode = .lines
         node.geometry?.firstMaterial?.transparency = 0.0
+        
         let noseNode = FilterNode(with: noseOptions)
         noseNode.name = "nose"
+        noseNode.scale = SCNVector3(0.9, 0.6, 0.7)
         node.addChildNode(noseNode)
         
-        
-        
+       
         let leftEyeNode = FilterNode(with: eyeOptions)
         leftEyeNode.name = "leftEye"
+        leftEyeNode.scale = SCNVector3(1.1, 0, 1.1)
         leftEyeNode.rotation = SCNVector4(0, 1, 0, GLKMathDegreesToRadians(180.0))
         node.addChildNode(leftEyeNode)
         
         let rightEyeNode = FilterNode(with: eyeOptions)
         rightEyeNode.name = "rightEye"
+        rightEyeNode.scale = SCNVector3(1.1, 0, 1.1)
         node.addChildNode(rightEyeNode)
         
         let mouthNode = FilterNode(with: mouthOptions)
         mouthNode.name = "mouth"
+        mouthNode.scale = SCNVector3(0.7, 0.4, 0.6)
         node.addChildNode(mouthNode)
         
         let hatNode = FilterNode(with: hatOptions)
         hatNode.name = "hat"
+        hatNode.scale = SCNVector3(2.7, 1.6, 2.5)
         node.addChildNode(hatNode)
         updateFeatures(for: node, using: faceAnchor)
         
