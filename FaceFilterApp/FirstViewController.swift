1c5a46c5ab0dad4e00487e613ee1ce33f423f687
Charmi Mehta <charmimehta15@gmail.com>
2019-04-22 16:24:06 -0400

    Menu screen added

---
+++ FaceFilterApp/FirstViewController.swift
@@ -0,0 +1,38 @@
+//
+//  FirstViewController.swift
+//  FaceFilterApp
+//
+//  Created by Charmi Mehta on 2019-04-22.
+//  Copyright © 2019 Abita Shiney. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func startBtn(_ sender: Any) {
+        let storyBoard : UIStoryboard = UIStoryboard(name: "FirstVC", bundle:nil)
+        let nextViewController = storyBoard.instantiateViewController(withIdentifier: "StartVC") as! ViewController
+        self.present(nextViewController, animated:true, completion:nil)
+    }
+    
+    @IBAction func tutorialBtn(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
