134949fa050dda38a5c63396563a4468055b262f
Charmi Mehta <charmimehta15@gmail.com>
2019-04-22 17:18:20 -0400

    Tutorial screen added

--- FaceFilterApp/FirstViewController.swift
+++ FaceFilterApp/FirstViewController.swift
@@ -18,12 +18,15 @@ class FirstViewController: UIViewController {
     
 
     @IBAction func startBtn(_ sender: Any) {
-        let storyBoard : UIStoryboard = UIStoryboard(name: "FirstVC", bundle:nil)
+        let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
         let nextViewController = storyBoard.instantiateViewController(withIdentifier: "StartVC") as! ViewController
         self.present(nextViewController, animated:true, completion:nil)
     }
     
     @IBAction func tutorialBtn(_ sender: Any) {
+        let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+        let nextViewController = storyBoard.instantiateViewController(withIdentifier: "TutorialVC") as! TutorialViewController
+        self.present(nextViewController, animated:true, completion:nil)
     }
     /*
     // MARK: - Navigation
